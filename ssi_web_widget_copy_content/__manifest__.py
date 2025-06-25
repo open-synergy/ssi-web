177d6b23826afc7bb9b39375b9b474150310b52f
Andhitia Rama <andhitia.r@gmail.com>
2025-06-25 15:49:31 +0700

    [ADD] ssi_web_widget_copy_content

---
+++ ssi_web_widget_copy_content/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Widget to Add Copy Button Beside Field",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/assets.xml",
+    ],
+    "demo": [],
+}
