04ac18bdc4e6e059a4d6482c5e7eb4c61fccdf2b
Andhitia Rama <andhitia.r@gmail.com>
2026-04-03 16:30:17 +0700

    [ADD] ssi_web_widget_csv_table

---
+++ ssi_web_widget_csv_table/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Widget CSV Table",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/assets.xml",
+    ],
+    "demo": [],
+}
