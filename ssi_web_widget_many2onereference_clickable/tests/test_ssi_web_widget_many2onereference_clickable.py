23afa218e81e359e789e15656996a2a5814d5c05
Andhitia Rama <andhitia.r@gmail.com>
2026-06-01 20:12:38 +0700

    [UPD] ssi_web_widget_many2onereference_clickable
    * Add unit test (smoke test - verify module installs)

---
+++ ssi_web_widget_many2onereference_clickable/tests/test_ssi_web_widget_many2onereference_clickable.py
@@ -0,0 +1,15 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestSsiWebWidgetMany2oneReferenceClickable(YamlTransactionCase):
+    def test_ssi_web_widget_many2onereference_clickable(self):
+        self.run_yaml_scenario(
+            "test_data_ssi_web_widget_many2onereference_clickable.yaml"
+        )
