f287920adba7fc8c6302b1cb23bb369c417a66fb
Andhitia Rama <andhitia.r@gmail.com>
2026-06-01 20:11:33 +0700

    [UPD] ssi_web_edit_attachment_name
    * Add unit test (smoke test - verify module installs)

---
+++ ssi_web_edit_attachment_name/tests/test_ssi_web_edit_attachment_name.py
@@ -0,0 +1,13 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestSsiWebEditAttachmentName(YamlTransactionCase):
+    def test_ssi_web_edit_attachment_name(self):
+        self.run_yaml_scenario("test_data_ssi_web_edit_attachment_name.yaml")
